ffc0f32e3805a2d5c032620bd8af16f8e453ca57
Hojjat Afsharan <afsharan.h@gmail.com>
2023-04-01 15:39:27 +0200

    add cypress and write a simple test for title in different screen sizes

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,17 @@
+describe('template spec', () => {
+
+  beforeEach(() => {
+  })
+
+  it('should contain title', () => {
+    cy.visit('localhost:4200');
+    cy.viewport("macbook-13");
+    cy.contains('The Cantina Crew: A Whimsical List of Star Wars Characters');
+  })
+  
+  it('should contain simpler title in small screens', () => {
+    cy.visit('localhost:4200');
+    cy.viewport("iphone-6");
+    cy.contains('The Cantina Crew');
+  })
+})
