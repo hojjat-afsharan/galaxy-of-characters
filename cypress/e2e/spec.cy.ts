76389721d8862daab672e86207b14d91bd2ef1f7
Hojjat Afsharan <afsharan.h@gmail.com>
2023-04-01 16:35:19 +0200

    add some e2e tests and debug session data

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('Star wars', () => {
 
   beforeEach(() => {
   })
@@ -8,7 +8,7 @@ describe('template spec', () => {
     cy.viewport("macbook-13");
     cy.contains('The Cantina Crew: A Whimsical List of Star Wars Characters');
   })
-  
+
   it('should contain simpler title in small screens', () => {
     cy.visit('localhost:4200');
     cy.viewport("iphone-6");
