d418a92dd3d964e4d2c0024345ffce3f8ebd6c6a
Hojjat Afsharan <afsharan.h@gmail.com>
2023-04-01 15:14:57 +0200

    extend people service unit tests

--- src/app/components/people/services/people.service.spec.ts
+++ src/app/components/people/services/people.service.spec.ts
@@ -1,29 +1,65 @@
 import { PeopleService } from "./people.service";
-import { describe, test, beforeEach, expect} from '@jest/globals';
+import { describe, test, beforeEach, expect } from "@jest/globals";
 import { of } from "rxjs";
+import { People, PeoplePageParams } from "../models/people.model";
 
-describe('PeopleService', () => {
+describe("PeopleService", () => {
+  let service: PeopleService;
+  let stateServiceMocked: any;
+  let dataService: any;
+  let mockPeople: People[] = [];
 
-    let service: PeopleService;
-    let stateServiceMocked: any;
-    let dataService: any;
+  beforeEach(() => {
+    stateServiceMocked = {};
+    dataService = {};
+    service = new PeopleService(stateServiceMocked, dataService);
+  });
 
+  test("should create", () => {
+    expect(service).toBeDefined();
+  });
+
+  describe("getPeople", () => {
     beforeEach(() => {
-        stateServiceMocked = {};
-        dataService = {};
-        service = new PeopleService(stateServiceMocked, dataService);
-    });  
+      mockPeople = [
+        new People({
+          uid: "1",
+          name: "Luke Skywalker",
+          url: "https://swapi.dev/api/people/1/",
+        }),
+        new People({
+          uid: "2",
+          name: "Leia Organa",
+          url: "https://swapi.dev/api/people/5/",
+        }),
+        new People({
+          uid: "3",
+          name: "Han Solo",
+          url: "https://swapi.dev/api/people/14/",
+        }),
+      ];
 
-    test('should create', () => {
-        expect(service).toBeDefined();
+      dataService.fetchPeople = jest.fn().mockReturnValue(of(mockPeople));
     });
 
-    test('should getPeople() return an Observable of type People[]', () => {
+    test("should getPeople() return an Observable of type People[]", () => {
+      service.getPeople().subscribe((data) => {
+        expect(Array.isArray(data)).toBeTruthy;
+        expect(data).toEqual(mockPeople);
+      });
+    });
 
-        dataService.fetchPeople = jest.fn().mockReturnValue(of({}));
+    test("should receive data of type People after calling getData", () => {
+      const params: PeoplePageParams = {
+        page: 1,
+        limit: 10,
+      };
 
-        service.getPeople().subscribe((data) => 
-        expect(Array.isArray(data)).toBeTruthy)
-    })
-    
-})
+      service.getData(params);
+
+      service.people$.subscribe((data: People[]) => {
+        expect(data).toBeInstanceOf(People);
+      });
+    });
+  });
+});
